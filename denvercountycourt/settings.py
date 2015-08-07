9ac13b13525ac003bced9acafc6dea073afdee80
Khlyestov Illarion <khlyestovillarion@gmail.com>
2015-08-07 21:56:44 +0300

    Change database connection settings

--- denvercountycourt/settings.py
+++ denvercountycourt/settings.py
@@ -11,7 +11,6 @@ DOWNLOAD_HANDLERS = {
   's3': None,
 }
 DUPEFILTER_DEBUG = True
-# CONCURRENT_REQUESTS_PER_DOMAIN = 1
 
 ITEM_PIPELINES = {
     'denvercountycourt.pipelines.DenvercountycourtPipeline': 300,
@@ -19,6 +18,22 @@ ITEM_PIPELINES = {
 
 CAPTCHA_USERNAME = os.getenv('CAPTCHA_USERNAME')
 CAPTHCA_PASSWORD = os.getenv('CAPTHCA_PASSWORD')
+
+DATABASE_USERNAME = os.getenv('DATABASE_USERNAME')
+DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD')
+
+DATABASE = {
+    'drivername': 'mysql',
+    'host': 'coloradorecords.c5bnots1acia.us-east-1.rds.amazonaws.com',
+    # 'host': 'localhost',
+    'port': '3306',
+    'username': DATABASE_USERNAME,
+    'password': DATABASE_PASSWORD,
+    'database': 'coloradorecords',
+    'query': {
+        'charset': 'utf8',
+    }
+}
 
 try:
     from settings_local import *
